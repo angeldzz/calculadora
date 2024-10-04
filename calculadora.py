4080c7979b9ae674afbac90d76b8f16492fc425c
angeldzz <131559735+angeldzz@users.noreply.github.com>
2024-10-04 08:51:52 +0200

    Cambios suma calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -2,7 +2,7 @@
 x = int(input("Introduzca numero 1"))
 y = int(input("Introduzca numero 2"))
 
-print(f"La suma de {x} y {y} es {x + y}")
+print(f"La suma modificada de {x} y {y} es {x + y}")
 print(f"La resta de {x} y {y} es {x - y}")
 print(f"La multiplicacion de {x} y {y} es {x * y}")
 print(f"La multiplicacion de {x} y {y} es {x / y}")
