b99251eaa59872a40c58e634ac86975da834d81a
angeldzz <131559735+angeldzz@users.noreply.github.com>
2024-10-04 08:45:03 +0200

    añadimos toda la calculadora otra vez

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+#!C:/Users/pinto/AppData/Local/Programs/Python/Python312/python.exe
+x = int(input("Introduzca numero 1"))
+y = int(input("Introduzca numero 2"))
+
+print(f"La suma de {x} y {y} es {x + y}")
+print(f"La resta de {x} y {y} es {x - y}")
+print(f"La multiplicacion de {x} y {y} es {x * y}")
